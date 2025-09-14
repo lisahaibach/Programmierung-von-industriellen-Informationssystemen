d526c8461e11ee9c24c4892374db384f9289aa03
Lisa Haibach <lhaibach.win-b2021@fh-salzburg.ac.at>
2025-09-14 19:13:33 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/de/iu/ghostnet/GhostNetApplication.java
@@ -0,0 +1,13 @@
+package de.iu.ghostnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhostNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhostNetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/iu/ghostnet/GhostNetApplicationTests.java
@@ -0,0 +1,13 @@
+package de.iu.ghostnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhostNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
